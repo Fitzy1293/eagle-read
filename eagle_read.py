668196de9987f080fd0b30e93f828ae558f85009
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2020-10-04 21:52:26 -0400

    Add files via upload

--- eagle_read.py
+++ eagle_read.py
@@ -1,22 +1,22 @@
 #!/bin/env python3
+'''
+Takes one argument:
+    Berkshire eagle article full URL
+Opens up the HTML in the browser
+'''
+
 import requests
 import sys
 import webbrowser
 from bs4 import BeautifulSoup
-from pprint import pprint
 
-
-def findPrintArticle(articleUrl='https://www.berkshireeagle.com') -> str:
-
+def findPrintArticle(articleUrl='https://www.berkshireeagle.com') -> str: #Returns article text as a string
     text = requests.get(articleUrl).text
     soup = BeautifulSoup(text, 'html.parser')
-
     paragraphs = [str(i) for i in soup.find_all('p') if str(i).startswith('<p>')]
-    #paragraphsStripTags = [i.replace('<p>', '').replace('</p>', '') for i in paragraphs]
-
     return '\n'.join(paragraphs)
 
-def writeHTML(fname, articleText) -> None:
+def writeHTML(fname, articleText) -> None: # Writes article to local html file
     with open(fname, 'w+') as f:
         f.write('<!DOCTYPE HTML>\n')
         f.write('<html>\n')
@@ -26,7 +26,6 @@ def writeHTML(fname, articleText) -> None:
         f.write('</html>\n')
 
 if __name__ == '__main__':
-    #articleUrl = 'https://www.berkshireeagle.com/stories/sterling-ready-to-put-her-toolkit-to-work-as-executive-director-of-the-massachusetts-black-and,614950'
     articleUrl = sys.argv[1]
     articleText = findPrintArticle(articleUrl)
     fname = articleUrl.split('/')[-1].replace('-', '_').replace(',', '_') + '.html'
