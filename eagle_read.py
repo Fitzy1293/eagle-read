791ba2eead82949c08e9db87a4e68b95c9933a19
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2020-10-04 21:43:35 -0400

    Add files via upload

--- eagle_read.py
+++ eagle_read.py
@@ -1,19 +1,34 @@
 #!/bin/env python3
 import requests
+import sys
+import webbrowser
 from bs4 import BeautifulSoup
 from pprint import pprint
 
 
-def findPrintArticle(articleUrl='https://www.berkshireeagle.com'):
+def findPrintArticle(articleUrl='https://www.berkshireeagle.com') -> str:
 
     text = requests.get(articleUrl).text
     soup = BeautifulSoup(text, 'html.parser')
 
     paragraphs = [str(i) for i in soup.find_all('p') if str(i).startswith('<p>')]
-    paragraphsStripTags = [i.replace('<p>', '').replace('</p>', '') for i in paragraphs]
+    #paragraphsStripTags = [i.replace('<p>', '').replace('</p>', '') for i in paragraphs]
 
-    return '\n'.join(paragraphsStripTags)
+    return '\n'.join(paragraphs)
 
-articleUrl = 'https://www.berkshireeagle.com/stories/sterling-ready-to-put-her-toolkit-to-work-as-executive-director-of-the-massachusetts-black-and,614950'
-readableArticle = findPrintArticle(articleUrl)
-print(readableArticle)
+def writeHTML(fname, articleText) -> None:
+    with open(fname, 'w+') as f:
+        f.write('<!DOCTYPE HTML>\n')
+        f.write('<html>\n')
+        f.write('<body>\n')
+        f.write(f'{articleText}\n')
+        f.write('</body>\n')
+        f.write('</html>\n')
+
+if __name__ == '__main__':
+    #articleUrl = 'https://www.berkshireeagle.com/stories/sterling-ready-to-put-her-toolkit-to-work-as-executive-director-of-the-massachusetts-black-and,614950'
+    articleUrl = sys.argv[1]
+    articleText = findPrintArticle(articleUrl)
+    fname = articleUrl.split('/')[-1].replace('-', '_').replace(',', '_') + '.html'
+    writeHTML(fname, articleText)
+    webbrowser.open(fname)
